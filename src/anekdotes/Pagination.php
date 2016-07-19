bfe96ce5f4d4b4e557e2d097df5810e64efafe19
Mathieu Gosselin <gosselin.mathieug@gmail.com>
2016-07-19 09:09:48 -0400

    inital commit and working pagination with test

---
+++ src/anekdotes/Pagination.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Anekdotes\Pagination;
+
+class Pagination
+{
+
+  public function pager($pages, $current, $length = 6)
+  {
+    if (intval($pages) == 0) {
+      return false;
+    }
+    if (intval($length) == 0) {
+      return false;
+    }
+
+    $datas = array();
+    $min = 1;
+    $max = $length > $pages ? $pages : $length;
+    $sub = floor($length/2);
+
+    if ($current > ($min+$sub)) {
+      $min = ($current-$sub);
+      $max = ($min+$length);
+      if ($max > $pages) {
+        $diff = $max-$pages;
+        $max = $pages;
+        $min -= $diff;
+        $min = $min < 1 ? 1 : $min;
+      }
+    }
+
+    for ($i = $min; $i <= $max; $i++) {
+      $datas[] = $i;
+    }
+
+    return $datas;
+  }
+}

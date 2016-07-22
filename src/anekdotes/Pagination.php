4fbc703fb2129ad1a02b6a0fcd572062f0ad167a
Mathieu Gosselin <gosselin.mathieug@gmail.com>
2016-07-22 11:07:19 -0400

    added documentation

--- src/anekdotes/Pagination.php
+++ src/anekdotes/Pagination.php
@@ -1,9 +1,29 @@
 <?php
+/*
+ * This file is part of the Pagination package.
+ *
+ * (c) Anekdotes Communication inc. <info@anekdotes.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Anekdotes\Pagination;
 
+/**
+ * Pagination class
+ */
 class Pagination
 {
+    /**
+     * Generates the pages to display according to its parameters
+     *
+     * @param integer $pages The total amount of page
+     * @param integer $current The current page
+     * @param integer $length The number of page to display
+     *
+     * @return array The pages to display
+     */
     public function pager($pages, $current, $length = 6)
     {
         if (intval($pages) == 0) {
